bd79a13db2fb4aca135b4ec45b4a774b86dca817
Brodypen <brodypen7987@gmail.com>
2023-09-16 13:31:27 -0500

    unearthed step 3

---
+++ unearthed/server/data/gifts.js
@@ -0,0 +1,11 @@
+const giftData = {
+    "id": 1,
+    "name": "Gift 1",
+    "pricePoint": 1,
+    "audience": "Children",
+    "image": "image1.jpg",
+    "description": "Description 1",
+    "submittedBy": "User 1",
+    "submittedOn": "2018-01-01"
+}
+export default giftData;
